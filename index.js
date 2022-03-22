855dd4a7bea11bee6621a9a4c03b99100dc32316
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2022-03-22 21:31:52 +0530

    appended 0's to the timer

--- index.js
+++ index.js
@@ -7,20 +7,33 @@ function startTimer(){
         startBtn.disabled = true;
         intervalId = setInterval( ()=> {
                 count++;
-                        result.innerHTML = Number.parseInt(count/60) + ":" + count % 60;
+                let seconds = (count % 60);
+                let minutes = parseInt((count/60)% 60);
+                let hours = parseInt(count/3600);
+
+                if(seconds < 10){
+                        seconds = "0" + seconds;
+                }
+                if(minutes < 10){
+                        minutes = "0" + minutes;
+                }
+                if(hours < 10){
+                        hours = "0" + hours;
+                }
+
+                result.innerHTML = hours + ":" + minutes + ":" + seconds ;
         },1000 )
 }
 
 function stopTimer(){
         clearInterval(intervalId);
         startBtn.removeAttribute("disabled");
-
 }
 
 function resetTimer(){
         clearInterval(intervalId);
         count = 0;
-        result.innerHTML = "00:00";
+        result.innerHTML = "00:00:00";
         startBtn.removeAttribute("disabled");
 }
 
